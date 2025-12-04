58e720fc5e7fb61fd74759681561f0b3ae9de619
voidnamespace <voidnamespace@outlook.com>
2025-12-04 17:46:02 +0400

    edited (formatted) code

--- AuthService/src/AuthService.API/Extensions/DependencyInjectionExtensions.cs
+++ AuthService/src/AuthService.API/Extensions/DependencyInjectionExtensions.cs
@@ -9,11 +9,9 @@ public static class DependencyInjectionExtensions
 {
     public static IServiceCollection AddApplicationServices(this IServiceCollection services)
     {
-        // Repositories
         services.AddScoped<IUserRepository, UserRepository>();
         services.AddScoped<IRefreshTokenRepository, RefreshTokenRepository>();
 
-        // Services
         services.AddScoped<IAuthService, AuthService.Infrastructure.Services.AuthService>();
         services.AddScoped<IJwtService, JwtService>();
 
